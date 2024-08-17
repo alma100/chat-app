d06f5b7b0f1038a9d807c296315c64e33ef98cc7
abaaba2 <grinacza@gmail.com>
2024-08-17 17:17:15 +0200

    update terraform variable.tf

--- Terraform/repository/variable.tf
+++ Terraform/repository/variable.tf
@@ -3,15 +3,3 @@ variable "aws_region" {
   type        = string
   default     = "us-east-1"
 }
-
-variable "aws_access_key" {
-  description = "AWS access key"
-  type        = string
-  sensitive   = true
-}
-
-variable "aws_secret_key" {
-  description = "AWS secret key"
-  type        = string
-  sensitive   = true
-}
