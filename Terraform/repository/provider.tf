534859bb16fa898f0691d2cba420b81dd761940c
abaaba2 <grinacza@gmail.com>
2024-08-17 19:04:33 +0200

    update provider.tf

--- Terraform/repository/provider.tf
+++ Terraform/repository/provider.tf
@@ -2,12 +2,12 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~>5.0"
     }
   }
   backend "s3" {
     bucket = "tf-bucket455454353453345" 
-    region = "us-west-1"                   
+    region = "us-east-1"                   
     key    = "terraform.tfstate"
   }
 }
