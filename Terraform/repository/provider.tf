6d10fc89c8995633004db8d413f9642fade6760a
abaaba2 <grinacza@gmail.com>
2024-08-17 17:24:47 +0200

    update provider.tf

--- Terraform/repository/provider.tf
+++ Terraform/repository/provider.tf
@@ -2,15 +2,14 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      Version = "~>3.27"
+      version = "~> 3.27"
     }
   }
 
   required_version = ">=0.14.9"
-
 }
 
 provider "aws" {
   version = "~>3.0"
-  region  = "east-us-1"
+  region  = "us-east-1"
 }
