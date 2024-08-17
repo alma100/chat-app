cf695d8aa4c1c9c4067d2d6e9bae6293aa40b460
abaaba2 <grinacza@gmail.com>
2024-08-17 17:40:05 +0200

    update provider.tf

--- Terraform/repository/provider.tf
+++ Terraform/repository/provider.tf
@@ -1,15 +1,31 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "~> 3.27"
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
   }
-
-  required_version = ">=0.14.9"
+  backend "s3" {
+    bucket = "tf-bucket455454353453345" 
+    region = "us-west-1"                   
+    key    = "terraform.tfstate"
+  }
 }
 
+
 provider "aws" {
-  version = "~>3.0"
-  region  = "us-east-1"
+  region = "us-east-1"
+}
+
+resource "aws_dynamodb_table" "questions" {
+  name           = "questions"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
 }
